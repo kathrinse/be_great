d651e181b91c2b13904b276da13e09c6d1b5fb2f
杨再润 <yangzair@outlook.com>
2023-12-23 15:21:14 +0800

    Update import paths in great.py

--- be_great_v/great.py
+++ be_great_v/great.py
@@ -12,16 +12,16 @@ from tqdm import tqdm
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
 
-from be_great.great_dataset import GReaTDataset, GReaTDataCollator
-from be_great.great_start import (
+from be_great_v.great_dataset import GReaTDataset, GReaTDataCollator
+from be_great_v.great_start import (
     GReaTStart,
     CategoricalStart,
     ContinuousStart,
     RandomStart,
     _pad_tokens,
 )
-from be_great.great_trainer import GReaTTrainer
-from be_great.great_utils import (
+from be_great_v.great_trainer import GReaTTrainer
+from be_great_v.great_utils import (
     _array_to_dataframe,
     _get_column_distribution,
     _convert_tokens_to_text,
